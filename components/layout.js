d156921f18885bbc228e39d70577f330c8413bc8
a2k11 <akaewell@xometry.com>
2020-10-29 09:48:02 -0400

    added postcss to transform css with javascript, tailwind css framework for custom designs, added alert component for success & failure events

--- components/layout.js
+++ components/layout.js
@@ -8,6 +8,59 @@ export const siteTitle = "Next.js Site"
 
 export default function Layout({ children, home }) {
   return (
-    <div className={styles.container}>{children}</div>
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Building a Next.js Site"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo-svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/profile.png"
+              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <img
+                  src="/images/profile.png"
+                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorinherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+     <main>{children}</main>
+     {!home && (
+       <div className={styles.backToHome}>
+         <Link href="/">
+           <a>Home</a>
+         </Link>
+        </div>
+      )}
+    </div>
   )
 }
