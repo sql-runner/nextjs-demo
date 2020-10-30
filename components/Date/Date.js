cc9f3c29ecb0ce7be2a39d060c23e57eda5155f2
a2k11 <akaewell@xometry.com>
2020-10-30 09:54:58 -0400

    fixed Alert component, added Date component, added 404 error page, removed unwanted post page, added dynamic routing of posts, polished up post page

---
+++ components/Date/Date.js
@@ -0,0 +1,12 @@
+import { parseISO, format } from 'date-fns'
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString)
+  return (
+    <time 
+      dateTime={dateString}
+    >
+      {format(date, 'MMMM do, yyyy')}
+    </time>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/Date'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +25,22 @@ export default function Home() {
         <p>
           Next.js Application
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
